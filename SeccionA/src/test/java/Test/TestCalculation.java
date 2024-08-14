74d4077029125bf909be18330a155154d2e76bad
Juan Munizaga Torres <juanmuni@espol.edu.ec>
2024-08-14 14:56:07 -0500

    SeccionA creacion clase calculator

---
+++ SeccionA/src/test/java/Test/TestCalculation.java
@@ -0,0 +1,13 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestCalculation {
+	@Test
+	void testFindMax() {
+		fail("Not yet implemented");
+	}
+
+}
