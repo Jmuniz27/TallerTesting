2bff8b554486a1389a057fb14aad3ee084df9612
isabellaim <iimartin@espol.edu.ec>
2024-08-15 21:10:22 -0500

    cambio de nombre de variables

--- SeccionA/src/test/java/Test/TestEmployee.java
+++ SeccionA/src/test/java/Test/TestEmployee.java
@@ -15,11 +15,11 @@ class TestEmployee {
     private static Employee supervisorCAD;
     private static Employee managerUSD;
     private static Employee managerCAD;
-    private static LocalDate currentDate;
+    private static LocalDate fechaActual;
 
     @BeforeAll
     public static void setup() {
-        currentDate = LocalDate.now();
+        fechaActual = LocalDate.now();
         
         workerUSD = new Employee(1000, "USD", 0, EmployeeType.Worker);
         workerCAD = new Employee(1000, "CAD", 0, EmployeeType.Worker);
@@ -34,86 +34,86 @@ class TestEmployee {
     // Casos de prueba para cs()
     @Test
     public void testWorkerUSDPar() {
-        assumeTrue(currentDate.getMonthValue() % 2 == 0); 
-        float expectedSalary = 1000;
-        assertEquals(expectedSalary, workerUSD.cs(), 0.01);
+        assumeTrue(fechaActual.getMonthValue() % 2 == 0); 
+        float expectativa = 1000;
+        assertEquals(expectativa, workerUSD.cs(), 0.01);
     }
 
     @Test
     public void testWorkerUSDImpar() {
-        assumeTrue(currentDate.getMonthValue() % 2 != 0); 
-        float expectedSalary = 1000 + (386.0F / 12 * 2);
-        assertEquals(expectedSalary, workerUSD.cs(), 0.01);
+        assumeTrue(fechaActual.getMonthValue() % 2 != 0); 
+        float expectativa = 1000 + (386.0F / 12 * 2);
+        assertEquals(expectativa, workerUSD.cs(), 0.01);
     }
 
     @Test
     public void testWorkerCADPar() {
-        assumeTrue(currentDate.getMonthValue() % 2 == 0); 
-        float expectedSalary = (float) (1000 * 0.95);
-        assertEquals(expectedSalary, workerCAD.cs(), 0.01);
+        assumeTrue(fechaActual.getMonthValue() % 2 == 0); 
+        float expectativa = (float) (1000 * 0.95);
+        assertEquals(expectativa, workerCAD.cs(), 0.01);
     }
 
     @Test
     public void testWorkerCADImpar() {
-        assumeTrue(currentDate.getMonthValue() % 2 != 0); 
-        float expectedSalary = (float) (1000 * 0.95) + (386.0F / 12 * 2);
-        assertEquals(expectedSalary, workerCAD.cs(), 0.01);
+        assumeTrue(fechaActual.getMonthValue() % 2 != 0); 
+        float expectativa = (float) (1000 * 0.95) + (386.0F / 12 * 2);
+        assertEquals(expectativa, workerCAD.cs(), 0.01);
     }
 
     @Test
     public void testSupervisorUSDPar() {
-        assumeTrue(currentDate.getMonthValue() % 2 == 0);
-        float expectedSalary = 2000 + (0.1F * 0.35F);
-        assertEquals(expectedSalary, supervisorUSD.cs(), 0.01);
+        assumeTrue(fechaActual.getMonthValue() % 2 == 0);
+        float expectativa = 2000 + (0.1F * 0.35F);
+        assertEquals(expectativa, supervisorUSD.cs(), 0.01);
     }
 
     @Test
     public void testSupervisorUSDImpar() {
-        assumeTrue(currentDate.getMonthValue() % 2 != 0); 
-        float expectedSalary = 2000 + (0.1F * 0.35F) + (386.0F / 12 * 2);
-        assertEquals(expectedSalary, supervisorUSD.cs(), 0.01);
+        assumeTrue(fechaActual.getMonthValue() % 2 != 0); 
+        float expectativa = 2000 + (0.1F * 0.35F) + (386.0F / 12 * 2);
+        assertEquals(expectativa, supervisorUSD.cs(), 0.01);
     }
 
     @Test
     public void testSupervisorCADPar() {
-        assumeTrue(currentDate.getMonthValue() % 2 == 0); 
-        float expectedSalary = (float) (2000 * 0.95) + (0.1F * 0.35F);
-        assertEquals(expectedSalary, supervisorCAD.cs(), 0.01);
+        assumeTrue(fechaActual.getMonthValue() % 2 == 0); 
+        float expectativa = (float) (2000 * 0.95) + (0.1F * 0.35F);
+        assertEquals(expectativa, supervisorCAD.cs(), 0.01);
     }
 
     @Test
     public void testSupervisorCADImpar() {
-        assumeTrue(currentDate.getMonthValue() % 2 != 0); 
-        float expectedSalary = (float) (2000 * 0.95) + (0.1F * 0.35F) + (386.0F / 12 * 2);
-        assertEquals(expectedSalary, supervisorCAD.cs(), 0.01);
+        assumeTrue(fechaActual.getMonthValue() % 2 != 0); 
+        float expectativa = (float) (2000 * 0.95) + (0.1F * 0.35F) + (386.0F / 12 * 2);
+        assertEquals(expectativa, supervisorCAD.cs(), 0.01);
     }
 
     @Test
     public void testManagerUSDPar() {
-        assumeTrue(currentDate.getMonthValue() % 2 == 0); 
-        float expectedSalary = 3000 + (0.2F * 0.7F);
-        assertEquals(expectedSalary, managerUSD.cs(), 0.01);
+        assumeTrue(fechaActual.getMonthValue() % 2 == 0); 
+        float expectativa = 3000 + (0.2F * 0.7F);
+        assertEquals(expectativa, managerUSD.cs(), 0.01);
     }
 
     @Test
     public void testManagerUSDImpar() {
-        assumeTrue(currentDate.getMonthValue() % 2 != 0); 
-        float expectedSalary = 3000 + (0.2F * 0.7F) + (386.0F / 12 * 2);
-        assertEquals(expectedSalary, managerUSD.cs(), 0.01);
+        assumeTrue(fechaActual.getMonthValue() % 2 != 0); 
+        float expectativa = 3000 + (0.2F * 0.7F) + (386.0F / 12 * 2);
+        assertEquals(expectativa, managerUSD.cs(), 0.01);
     }
 
     @Test
     public void testManagerCADPar() {
-        assumeTrue(currentDate.getMonthValue() % 2 == 0); 
-        float expectedSalary = (float) (3000 * 0.95) + (0.2F * 0.7F);
-        assertEquals(expectedSalary, managerCAD.cs(), 0.01);
+        assumeTrue(fechaActual.getMonthValue() % 2 == 0); 
+        float expectativa = (float) (3000 * 0.95) + (0.2F * 0.7F);
+        assertEquals(expectativa, managerCAD.cs(), 0.01);
     }
 
     @Test
     public void testManagerCADImpar() {
-        assumeTrue(currentDate.getMonthValue() % 2 != 0);
-        float expectedSalary = (float) (3000 * 0.95) + (0.2F * 0.7F) + (386.0F / 12 * 2);
-        assertEquals(expectedSalary, managerCAD.cs(), 0.01);
+        assumeTrue(fechaActual.getMonthValue() % 2 != 0);
+        float expectativa = (float) (3000 * 0.95) + (0.2F * 0.7F) + (386.0F / 12 * 2);
+        assertEquals(expectativa, managerCAD.cs(), 0.01);
     }
 
     // Casos de prueba para CalculateYearBonus()
